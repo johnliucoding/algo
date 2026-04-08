30e2cde21d0111658b666a7e7f81efb42ca66585
liuxiaodong02 <liuxiaodong02@inspur.com>
2026-04-08 14:01:11 +0800

    permutation backtrack

--- src/main/java/coding/pattern/chap14/Permutation.java
+++ src/main/java/coding/pattern/chap14/Permutation.java
@@ -1,8 +1,6 @@
 package coding.pattern.chap14;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author liuxiaodong02
@@ -12,11 +10,15 @@ public class Permutation {
     public static List<int[]> permutation(int[] nums) {
         var result = new ArrayList<int[]>();
 
-       backtrack(nums, new ArrayList<Integer>(), new HashSet<Integer>(), result);
+//       backtrack(nums, new ArrayList<>(), new HashSet<>(), result);
+       permutate(nums, 0, nums.length - 1, result);
         return result;
     }
 
-    private static void backtrack(int[] nums, ArrayList<Integer> candidate, HashSet<Integer> used, ArrayList<int[]> result) {
+    private static void backtrack(int[] nums,
+                                  List<Integer> candidate,
+                                  Set<Integer> used,
+                                  List<int[]> result) {
         // if the current candidate is a complete permutation, add it to the result
         if(candidate.size() == nums.length) {
             result.add(candidate.stream().mapToInt(i -> i).toArray());
@@ -31,9 +33,30 @@ public class Permutation {
                 // recursively explore all branches using the updated permutation candidate
                 backtrack(nums, candidate, used, result);
                 // backtrack by reversing the change made
-                candidate.remove(candidate.size()-1);
+                candidate.removeLast();
                 used.remove(num);
             }
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private static void permutate(int[] nums, int left, int right, List<int[]> result) {
+        if(left == right) {
+            result.add(Arrays.copyOf(nums, nums.length));
+            return;
+        }
+        for (int i = left; i <= right; i++) {
+            // select one element 'i' to occupy left position
+            swap(nums, left, i);
+            // recur on rest position and element
+            permutate(nums, left + 1, right, result);
+            // back track
+            swap(nums, left, i);
         }
     }
 }
